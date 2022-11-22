387fff66075dd31d540b0f8b1d63bed0f9f9f0de
John-Yip-HK <yip4work@gmail.com>
2022-11-22 23:01:49 +0800

    multiple page test

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -10,21 +10,32 @@ describe("empty spec", () => {
     cy.visit("http://localhost:3000")
   })
 
-  it("passes", () => {
-    // get(): retrieve element
-    // contains(): an assertion to check if the contained text in the element matches the one the user provides
-    // Getting elements best practice in Cypress: using data attributes on your elements
-    cy.getByData("hero-heading").contains(
-      "Testing Next.js Applications with Cypress"
-    )
+  // context() allows us to group related tests together. It is an alias of describe()
+  context("Hero section", () => {
+    it("the h1 contains the correct text", () => {
+      // get(): retrieve element
+      // contains(): an assertion to check if the contained text in the element matches the one the user provides
+      // Getting elements best practice in Cypress: using data attributes on your elements
+      cy.getByData("hero-heading").contains(
+        "Testing Next.js Applications with Cypress"
+      )
+    })
+
+    // Sometimes we cannot modify the underlying HTML of 3rd-party library code and add custom data-* attributes.
+    it("the features on the homepage are correct", () => {
+      // eq(): access a specific index within an array of elements
+      // Bear in mind that contains() is case-sensitive. Thus, entering "4 courses" will fail the test.
+      cy.get("dt").eq(0).contains("4 Courses")
+      cy.get("dt").eq(1).contains("25+ Lessons")
+      cy.get("dt").eq(2).contains("Free and Open Source")
+    })
   })
 
-  // Sometimes we cannot modify the underlying HTML of 3rd-party library code and add custom data-* attributes.
-  it("the features on the homepage are correct", () => {
-    // eq(): access a specific index within an array of elements
-    // Bear in mind that contains() is case-sensitive. Thus, entering "4 courses" will fail the test.
-    cy.get("dt").eq(0).contains("4 Courses")
-    cy.get("dt").eq(1).contains("25+ Lessons")
-    cy.get("dt").eq(2).contains("Free and Open Source")
+  context.only("course selection", () => {
+    it("Course: Testing Your First Next.js Application", () => {
+      // find(): narrow down the scope in which Cypress looks for an element.
+      // Find the specified elemenet within another element.
+      cy.getByData("course-0").find("a").eq(3).click()
+    })
   })
 })
